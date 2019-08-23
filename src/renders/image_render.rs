18b951877b16b235772f8c3ee13bb4bdd52326d8
carrotflakes <carrotflakes@gmail.com>
2019-08-24 04:51:25 +0900

    fix image_render

--- src/renders/image_render.rs
+++ src/renders/image_render.rs
@@ -14,12 +14,12 @@ pub struct ImageRender<T> {
 
 impl <T: Default + Clone> Render<T> for ImageRender<T> {
     fn sample(&self, u: f64, v: f64, time: f64) -> T {
-        if (0.0..=1.0).contains(&u) && (0.0..=1.0).contains(&v) {
-            let width = self.image.width;
-            let height = self.image.height;
-            let x = (u * width as f64).floor() as usize;
-            let y = (v * height as f64).floor() as usize;
-            self.image.vec[y * width + x].clone()
+        let width = self.image.width;
+        let height = self.image.height;
+        let x = (u * width as f64).floor() as i32;
+        let y = (v * height as f64).floor() as i32;
+        if (0..width as i32).contains(&x) && (0..height as i32).contains(&y) {
+            self.image.vec[y as usize * width + x as usize].clone()
         } else {
             T::default()
         }
