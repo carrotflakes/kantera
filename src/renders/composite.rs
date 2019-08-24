0416503ce0732f897c04234a534a84ac145331c4
carrotflakes <carrotflakes@gmail.com>
2019-08-24 16:07:46 +0900

    composite faster

--- src/renders/composite.rs
+++ src/renders/composite.rs
@@ -15,33 +15,42 @@ impl Render<Rgba> for Composite {
     fn sample(&self, u: f64, v: f64, time: f64) -> Rgba {
         let mut value = Rgba::default();
         for (render, cm) in &self.layers {
-            composite(&mut value, &render.sample(u, v, time), time, cm);
+            composite(&mut [value], &[render.sample(u, v, time)], time, cm);
         }
         value
     }
 
     fn render(&self, ro: &RenderOpt, buffer: &mut [Rgba]) {
-        let RenderOpt {u_range, u_res, v_range, v_res, frame_range, framerate, ..} = ro;
+        let RenderOpt {u_res, v_res, frame_range, framerate, ..} = ro;
         let mut sub_buffer = vec![Rgba::default(); buffer.len()];
+        let frame_size = u_res * v_res;
 
         for (render, cm) in &self.layers {
             render.render(ro, sub_buffer.as_mut_slice());
             for f in frame_range.start..frame_range.end {
-                let time = f as f64 / *framerate as f64;
-                for xy in 0..(*v_res * *u_res) {
-                    let i = (f - frame_range.start) as usize * u_res * v_res + xy;
-                        composite(&mut buffer[i], &sub_buffer[i], time, cm);
-                }
+                let start = frame_size * (f - frame_range.start) as usize;
+                composite(
+                    &mut buffer[start..start+frame_size],
+                    &sub_buffer[start..start+frame_size],
+                    f as f64 / *framerate as f64, cm);
             }
         }
     }
 }
 
 #[inline(always)]
-fn composite(base: &mut Rgba, value: &Rgba, time: f64, cm: &CompositeMode) {
-    *base = match cm {
-        CompositeMode::None => *value,
-        CompositeMode::Normal(alpha_path) =>
-            base.normal_blend(value, alpha_path.get_value(time))
+fn composite(base: &mut [Rgba], value: &[Rgba], time: f64, cm: &CompositeMode) {
+    match cm {
+        CompositeMode::None => {
+            for i in 0..base.len() {
+                base[i] = value[i];
+            }
+        },
+        CompositeMode::Normal(alpha_path) => {
+            let alpha = alpha_path.get_value(time);
+            for i in 0..base.len() {
+                base[i] = base[i].normal_blend(&value[i], alpha);
+            }
+        }
     };
 }
