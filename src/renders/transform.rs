d4637b18a23a68e5833cc13c607d1234b7c9f189
carrotflakes <carrotflakes@gmail.com>
2019-09-06 04:25:42 +0900

    add camera_shake2

--- src/renders/transform.rs
+++ src/renders/transform.rs
@@ -1,4 +1,5 @@
 use crate::render::{Res, Render};
+use crate::util::noise;
 
 pub struct Transform<T> {
     pub render: Box<Render<T>>,
@@ -18,6 +19,17 @@ pub fn camera_shake(size: f64) -> Box<Fn(f64, f64, f64, Res) -> (f64, f64, f64)>
         (
             u + (r * 0.523 + (r * 2.0).sin() * 3.0).sin() * r.cos() * size,
             v + (r * 0.525 + (r * 2.1).sin() * 3.0).sin() * (r * 1.001).cos() * size,
+            time
+        )
+    })
+}
+
+pub fn camera_shake2(size: f64, time_scale: f64) -> Box<Fn(f64, f64, f64, Res) -> (f64, f64, f64)> {
+    Box::new(move |u, v, time, res| {
+        let r = time;
+        (
+            u + noise(0.0, 0.0, time * time_scale) / res.0 as f64 * size,
+            v + noise(0.5, 2.0, time * time_scale) / res.1 as f64 * size,
             time
         )
     })
