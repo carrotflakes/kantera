906b22db9cef2edd27f4006b7ac8999f9be22b10
carrotflakes <carrotflakes@gmail.com>
2019-09-28 07:07:21 +0900

    add text.rs (not tested yet!)

---
+++ src/text.rs
@@ -0,0 +1,42 @@
+extern crate rusttype;
+
+pub use rusttype::*;
+use crate::pixel::Rgba;
+use crate::image::Image;
+
+pub fn render(font: &Font, text: &str) -> Buffer<Rgba> {
+    let scale = Scale::uniform(32.0);
+    let v_metrics = font.v_metrics(scale);
+
+    let glyphs_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
+    let glyphs_width = {
+        let min_x = glyphs
+            .first()
+            .map(|g| g.pixel_bounding_box().unwrap().min.x)
+            .unwrap();
+        let max_x = glyphs
+            .last()
+            .map(|g| g.pixel_bounding_box().unwrap().max.x)
+            .unwrap();
+        (max_x - min_x) as u32
+    };
+
+    let (width, height) = (glyphs_width as usize, glyphs_height as usize);
+    let mut vec = vec![Rgba(1.0, 1.0, 1.0, 0.0); width * height];
+
+    for glyph in font.layout(text, scale, point(20.0, 20.0 + v_metrics.ascent)) {
+        if let Some(bounding_box) = glyph.pixel_bounding_box() {
+            glyph.draw(|x, y, v| {
+                let x = x as usize + bounding_box.min.x as usize;
+                let y = y as usize + bounding_box.min.y as usize;
+                vec[x + width * y] = Rgba(1.0, 1.0, 1.0, 1.0);
+            });
+        }
+    }
+
+    Image {
+        width,
+        height,
+        vec
+    }
+}
