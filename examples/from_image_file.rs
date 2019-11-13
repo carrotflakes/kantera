150540d1dfd7617fa9b2b3adc754e2e75cf69400
carrotflakes <carrotflakes@gmail.com>
2019-11-13 21:18:05 +0900

    add example/from_image_file.rs

---
+++ examples/from_image_file.rs
@@ -0,0 +1,37 @@
+extern crate kantera;
+extern crate image;
+
+use std::rc::Rc;
+use kantera::pixel::Rgba;
+use kantera::image::Image;
+use kantera::render::{Range, Render, RenderOpt};
+use kantera::export::render_to_mp4;
+use kantera::renders::{
+    image_render::{ImageRender, Sizing}
+};
+
+fn main() {
+    let (width, height) = (320, 240);
+
+    let img = Rc::new({
+        let buf = image::open("./out.jpg").unwrap().to_rgba();
+        Image {
+            width: buf.width() as usize,
+            height: buf.height() as usize,
+            vec: buf.pixels().map(|image::Rgba([r, g, b, a])| {
+                Rgba(
+                    *r as f64 / std::u8::MAX as f64,
+                    *g as f64 / std::u8::MAX as f64,
+                    *b as f64 / std::u8::MAX as f64,
+                    *a as f64 / std::u8::MAX as f64)
+            }).collect()
+        }
+    });
+
+    render_to_mp4(
+        5.0, width, height, 30, 1,
+        "from_image_file.mp4",
+        &ImageRender {image: img.clone(), sizing: Sizing::Contain});
+
+    println!("done!");
+}
