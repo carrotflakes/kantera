779dd3ef4c030d7e6a0ac3cc8cea736657151b87
carrotflakes <carrotflakes@gmail.com>
2019-12-15 21:30:07 +0900

    fix examples/script.rs

--- examples/script.rs
+++ examples/script.rs
@@ -1,6 +1,3 @@
-//#[macro_use]
-//extern crate gluten;
-
 use kantera::pixel::Rgba;
 use kantera::render::Render;
 use kantera::export::render_to_mp4;
@@ -16,12 +13,11 @@ fn main() {
             (vec (parse_f64 '2.0) true (plain (rgba (parse_f64 '0.0) (parse_f64 '0.0) (parse_f64 '1.0) (parse_f64 '1.0))))
         )"
     ).unwrap());
-    let mut res = res.borrow_mut();
-    let render = res.downcast_mut::<Option<Rc<dyn Render<Rgba>>>>().unwrap().as_mut().unwrap();
+    let render = res.borrow().downcast_ref::<Option<Rc<dyn Render<Rgba>>>>().unwrap().as_ref().unwrap().clone();
     render_to_mp4(
         5.0, 320, 240, 30, 1,
         "script.mp4",
-        render);
+        &render);
 
     println!("done!");
 }
