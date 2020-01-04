58a631c5dc5925e71ac758c7658117014259f084
carrotflakes <carrotflakes@gmail.com>
2020-01-05 01:48:18 +0900

    [web-ui] refactor

---
+++ kantera-web-ui/src/my_websocket.rs
@@ -0,0 +1,132 @@
+use std::sync::Mutex;
+use std::time::{Duration, Instant};
+
+use actix::prelude::*;
+use actix_web_actors::ws;
+
+use kantera::{
+    pixel::Rgba,
+    render::{Render, Dummy, RenderOpt, Range},
+    export::render_to_buffer,
+    script::*
+};
+use std::rc::Rc;
+
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+const FRAMERATE_DEFAULT: usize = 30;
+
+pub struct MyWebSocket {
+    hb: Instant,
+    frame: Mutex<i32>,
+    render: Rc<dyn Render<Rgba>>,
+    framerate: usize,
+    render_at: Instant
+}
+
+impl Actor for MyWebSocket {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.haertbeat(ctx);
+        self.render_loop(ctx);
+    }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
+    fn handle(
+        &mut self,
+        msg: Result<ws::Message, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        println!("WS: {:?}", msg);
+        match msg {
+            Ok(ws::Message::Ping(msg)) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            Ok(ws::Message::Pong(_)) => {
+                self.hb = Instant::now();
+            }
+            Ok(ws::Message::Text(text)) => {
+                println!("{:?}", text);
+                let mut reader = make_reader();
+                match reader.parse_top_level(&text) {
+                    Ok(mut forms) => {
+                        forms.insert(0, reader.parse("do").unwrap());
+                        let mut env = make_env();
+                        env.insert("framerate".to_string(), r(FRAMERATE_DEFAULT as i32));
+                        let res = eval(env.clone(), r(forms));
+                        self.render = res.borrow().downcast_ref::<Rc<dyn Render<Rgba>>>().unwrap().clone();
+                        if let Some(val) = env.get(&"framerate".to_string()) {
+                            let framerate = *val.borrow().downcast_ref::<i32>().unwrap();
+                            self.framerate = framerate.min(120).max(1) as usize;
+                        }
+                        *self.frame.lock().unwrap() = 0;
+                    },
+                    Err(mes) => ctx.text(format!(r#"{{"type":"parseFailed","error":{:?}}}"#, mes))
+                }
+            },
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(_)) => ctx.stop(),
+            _ => ctx.stop(),
+        }
+    }
+}
+
+impl MyWebSocket {
+    pub fn new() -> Self {
+        Self {
+            hb: Instant::now(),
+            frame: Mutex::new(0),
+            render: Rc::new(Dummy()),
+            framerate: 30,
+            render_at: Instant::now()
+        }
+    }
+
+    fn haertbeat(&self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                println!("Websocket Client heartbeat failed, disconnecting!");
+                ctx.stop();
+                return;
+            }
+            ctx.ping(b"");
+        });
+    }
+
+    fn render_loop(&mut self, ctx: &mut <Self as Actor>::Context) {
+        let (width, height) = (600usize, 400usize);
+        let frame = {
+            let mut frame = self.frame.lock().unwrap();
+            *frame += 1;
+            *frame - 1
+        };
+        let buffer = render_to_buffer(&RenderOpt {
+            u_range: Range::unit(),
+            u_res: width,
+            v_range: Range::unit(),
+            v_res: height,
+            frame_range: frame..frame+1,
+            framerate: self.framerate
+        }, &self.render);
+        let mut buf: Vec<u8> = vec![0; buffer.vec.len() * 4]; 
+        for i in 0..buffer.vec.len() {
+            buf[i * 4 + 0] = (buffer.vec[i].0.min(1.0).max(0.0) * 255.99).floor() as u8;
+            buf[i * 4 + 1] = (buffer.vec[i].1.min(1.0).max(0.0) * 255.99).floor() as u8;
+            buf[i * 4 + 2] = (buffer.vec[i].2.min(1.0).max(0.0) * 255.99).floor() as u8;
+            buf[i * 4 + 3] = (buffer.vec[i].3.min(1.0).max(0.0) * 255.99).floor() as u8;
+        }
+        let mut vec = Vec::new();
+        image::png::PNGEncoder::new(&mut vec).encode(&buf, width as u32, height as u32, image::RGBA(8)).unwrap();
+        ctx.binary(vec);
+        ctx.text(format!(r#"{{"type":"sync","frame":{}}}"#, frame));
+
+        let desire_duration = Duration::from_millis(1000 / self.framerate as u64);
+        self.render_at = (self.render_at + desire_duration).max(Instant::now() - desire_duration);
+        let duration = self.render_at.checked_duration_since(Instant::now()).unwrap_or(Duration::from_millis(1));
+        ctx.run_later(duration, Self::render_loop);
+    }
+}
