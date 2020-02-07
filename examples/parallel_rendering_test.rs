72d11da3acdc838c441286651253da464298a3f9
carrotflakes <carrotflakes@gmail.com>
2020-02-08 04:19:18 +0900

    make render_to_buffer_parallel generic

--- examples/parallel_rendering_test.rs
+++ examples/parallel_rendering_test.rs
@@ -2,6 +2,7 @@ extern crate kantera;
 
 use kantera::pixel::Rgba;
 use kantera::export::{render_to_buffer, render_to_buffer_parallel};
+use kantera::buffer::Buffer;
 use kantera::renders::sample::Sample;
 use kantera::render::{Range, Render, RenderOpt};
 use kantera::util::noise;
@@ -22,8 +23,12 @@ fn main() {
             noise(u / 10.0 * w as f64, v / 10.0 * h as f64, time) * 0.5 + 0.5, 1.0)
     }));
     for _ in 0..5 {
+        let start = std::time::Instant::now();
         render_to_buffer(&ro, &render);
-        render_to_buffer_parallel(&ro, unsafe { std::mem::transmute::<&dyn Render<Rgba>, &'static (dyn Render<Rgba> + Send + Sync)>(&render) });
+        println!("{:?}", start.elapsed());
+        let start = std::time::Instant::now();
+        render_to_buffer_parallel(&ro, unsafe { std::mem::transmute::<&dyn Render<Rgba>, &'static (dyn Render<Rgba> + Send + Sync)>(&render) }) as Buffer<Rgba>;
+        println!("{:?}", start.elapsed());
     }
 
     println!("done!");
--- src/export.rs
+++ src/export.rs
@@ -78,12 +78,7 @@ pub fn render_to_buffer<T: Default + Clone>(ro: &RenderOpt, render: &dyn Render<
 
 use std::thread;
 
-pub fn render_to_buffer_parallel<T: Default + Clone + Send>(ro: &RenderOpt, render: &'static (dyn Render<T> + Send + Sync)) -> Buffer<T> {
-    if unsafe {DEBUG_PRINT} {
-        println!("render start: {:#?}", ro);
-    }
-    let start = std::time::Instant::now();
-
+pub fn render_to_buffer_parallel<T: Default + Clone + Send, U: From<T>>(ro: &RenderOpt, render: &'static (dyn Render<T> + Send + Sync)) -> Buffer<U> {
     let frame_num = (ro.frame_range.end - ro.frame_range.start) as usize;
     let n = 4; // TODO
     let handles: Vec<_> = (0..n).map(|i| {
@@ -101,21 +96,15 @@ pub fn render_to_buffer_parallel<T: Default + Clone + Send>(ro: &RenderOpt, rend
     }).collect::<Vec<_>>();
     let vecs = handles.into_iter().map(|handle| handle.join().unwrap()).collect::<Vec<Vec<T>>>();
 
-    let mut vec = vec![T::default(); ro.u_res * ro.v_res * frame_num];
+    let mut vec = Vec::with_capacity(ro.u_res * ro.v_res * frame_num);
     for f in 0..frame_num {
-        for x in 0..ro.u_res {
-            for y in 0..ro.v_res {
+        for y in 0..ro.v_res {
+            for x in 0..ro.u_res {
                 let i = x * n / ro.u_res;
                 let u_res = (i + 1) * ro.u_res / n - i * ro.u_res / n;
-                vec[f * ro.u_res * ro.v_res + y * ro.u_res + x] = vecs[i][f * u_res * ro.v_res + y * u_res + (x - i * ro.u_res / n)].clone();
+                vec.push(vecs[i][f * u_res * ro.v_res + y * u_res + (x - i * ro.u_res / n)].clone().into());
             }
         }
-    }
-
-    let duration = start.elapsed();
-    if unsafe {DEBUG_PRINT} {
-        println!("render end, took: {}.{:04} sec",
-                duration.as_secs(), duration.subsec_nanos() / 1_000_000);
     }
 
     Buffer {
