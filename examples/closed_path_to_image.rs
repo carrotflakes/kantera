a7e89e8d82905670f1ad47d58b2518186be5c933
carrotflakes <carrotflakes@gmail.com>
2020-02-17 16:26:43 +0900

    implement path_to_image::{closed_path_rect, expand_rect}

--- examples/closed_path_to_image.rs
+++ examples/closed_path_to_image.rs
@@ -3,7 +3,7 @@ extern crate kantera;
 use std::rc::Rc;
 use kantera::export::render_to_mp4;
 use kantera::renders::image_render::{ImageRender, Sizing};
-use kantera::path_to_image::closed_path_to_image;
+use kantera::path_to_image::{closed_path_to_image, closed_path_rect, expand_rect};
 use kantera::pixel::Rgba;
 use kantera::v::Vec2;
 use kantera::path::{Path, Point};
@@ -12,7 +12,8 @@ fn main() {
     let path = Path::new(Vec2(1.0, 1.0))
         .append(1.0, Vec2(90.0, 50.0), Point::Linear)
         .append(1.0, Vec2(1.0, 90.0), Point::Bezier(Vec2(0.0, 20.0), Vec2(0.0, -30.0)));
-    let image = Rc::new(closed_path_to_image((-10, -10, 100, 100), Rgba(1.0, 0.0, 0.0, 1.0), Rgba(0.1, 0.7, 0.0, 1.0), 3.0, &path));
+    let rect = expand_rect(closed_path_rect(&path), 5);
+    let image = Rc::new(closed_path_to_image(rect, Rgba(1.0, 0.0, 0.0, 1.0), Rgba(0.1, 0.7, 0.0, 1.0), 3.0, &path));
     render_to_mp4(
         5.0,
         320,
--- src/path_to_image.rs
+++ src/path_to_image.rs
@@ -4,10 +4,10 @@ use crate::pixel::Rgba;
 use crate::image::Image;
 use crate::cairo::{render_image, Context};
 
-pub fn closed_path_to_image(rect: (i32, i32, i32, i32), stroke_color: Rgba, fill_color: Rgba, line_width: f64, path: &Path<Vec2<f64>>) -> Image<Rgba> {
+pub fn closed_path_to_image(rect: [i32; 4], stroke_color: Rgba, fill_color: Rgba, line_width: f64, path: &Path<Vec2<f64>>) -> Image<Rgba> {
     let builder = |ctx: Context| {
         ctx.set_line_width(line_width);
-        ctx.translate(-rect.0 as f64, -rect.1 as f64);
+        ctx.translate(-rect[0] as f64, -rect[1] as f64);
         ctx.move_to((path.points[0].1).0, (path.points[0].1).1);
         for pair in path.points.windows(2) {
             let prev = pair[0];
@@ -24,5 +24,42 @@ pub fn closed_path_to_image(rect: (i32, i32, i32, i32), stroke_color: Rgba, fill
         ctx.set_source_rgba(stroke_color.0, stroke_color.1, stroke_color.2, stroke_color.3);
         ctx.stroke();
     };
-    render_image((rect.2 - rect.0) as usize, (rect.3 - rect.1) as usize, &builder)
+    render_image((rect[2] - rect[0]) as usize, (rect[3] - rect[1]) as usize, &builder)
+}
+
+pub fn closed_path_rect(path: &Path<Vec2<f64>>) -> [i32; 4] {
+    let mut rect = [0f64, 0.0, 0.0, 0.0];
+    rect[0] = (path.points[0].1).0;
+    rect[1] = (path.points[0].1).1;
+    rect[2] = (path.points[0].1).0;
+    rect[3] = (path.points[0].1).1;
+    for pair in path.points.windows(2) {
+        let prev = pair[0];
+        let p = pair[1];
+        match p.2 {
+            Point::Constant => { // ?
+                rect[0] = rect[0].min((p.1).0);
+                rect[1] = rect[1].min((p.1).1);
+                rect[2] = rect[2].max((p.1).0);
+                rect[3] = rect[3].max((p.1).1);
+            },
+            Point::Linear => {
+                rect[0] = rect[0].min((p.1).0);
+                rect[1] = rect[1].min((p.1).1);
+                rect[2] = rect[2].max((p.1).0);
+                rect[3] = rect[3].max((p.1).1);
+            },
+            Point::Bezier(lh, rh) => {
+                rect[0] = rect[0].min((prev.1).0).min((prev.1).0 + lh.0).min((p.1).0 + rh.0).min((p.1).0);
+                rect[1] = rect[1].min((prev.1).1).min((prev.1).1 + lh.1).min((p.1).1 + rh.1).min((p.1).1);
+                rect[2] = rect[2].max((prev.1).0).max((prev.1).0 + lh.0).max((p.1).0 + rh.0).max((p.1).0);
+                rect[3] = rect[3].max((prev.1).1).max((prev.1).1 + lh.1).max((p.1).1 + rh.1).max((p.1).1);
+            }
+        }
+    }
+    [rect[0].floor() as i32, rect[1].floor() as i32, rect[2].ceil() as i32, rect[3].ceil() as i32]
+}
+
+pub fn expand_rect(rect: [i32; 4], size: i32) -> [i32; 4] {
+    [rect[0] - size, rect[1] - size, rect[2] + size, rect[3] + size]
 }
