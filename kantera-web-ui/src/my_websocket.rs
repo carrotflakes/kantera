f7ab08bf7c915a3bd7716fdea16e7f3f870bfb07
carrotflakes <carrotflakes@gmail.com>
2020-02-26 00:57:05 +0900

    [web-ui] support 'start_frame', 'end_frame' and 'loop' parameters

--- kantera-web-ui/src/my_websocket.rs
+++ kantera-web-ui/src/my_websocket.rs
@@ -22,12 +22,14 @@ const SAMPLERATE_DEFAULT: usize = 16000;
 
 pub struct MyWebSocket {
     hb: Instant,
-    frame: i32,
+    current_frame: Option<i32>,
     render: Option<Rc<dyn Render<Rgba>>>,
     audio_render: Option<Rc<dyn AudioRender>>,
     framerate: usize,
     samplerate: usize,
-    frame_num: Option<usize>,
+    start_frame: i32,
+    end_frame: Option<i32>,
+    loop_: bool,
     size: (usize, usize),
     render_at: Instant,
     rt_cache: Val
@@ -76,12 +78,14 @@ impl MyWebSocket {
     pub fn new() -> Self {
         Self {
             hb: Instant::now(),
-            frame: 0,
+            current_frame: None,
             render: None,
             audio_render: None,
             framerate: FRAMERATE_DEFAULT,
             samplerate: SAMPLERATE_DEFAULT,
-            frame_num: None,
+            start_frame: 0,
+            end_frame: None,
+            loop_: false,
             size: (600, 400),
             render_at: Instant::now(),
             rt_cache: r(HashMap::<String, Val>::new())
@@ -100,58 +104,65 @@ impl MyWebSocket {
     }
 
     fn render_loop(&mut self, ctx: &mut <Self as Actor>::Context) {
-        let frame = self.frame;
-        if let Some(ref render) = self.render {
-            let (width, height) = self.size;
-            let buffer = {
-                let render = unsafe { std::mem::transmute::<&dyn Render<Rgba>, &'static (dyn Render<Rgba> + Send + Sync)>(render) };
-                render_to_buffer_parallel(&RenderOpt {
-                    x_range: 0..width as i32,
-                    y_range: 0..height as i32,
-                    res_x: width,
-                    res_y: height,
-                    frame_range: frame..frame+1,
-                    framerate: self.framerate
-                }, render) as Buffer<Rgba>
-            };
-            let mut buf: Vec<u8> = vec![0; buffer.vec.len() * 4];
-            for i in 0..buffer.vec.len() {
-                buf[i * 4 + 0] = (buffer.vec[i].0.min(1.0).max(0.0) * 255.99).floor() as u8;
-                buf[i * 4 + 1] = (buffer.vec[i].1.min(1.0).max(0.0) * 255.99).floor() as u8;
-                buf[i * 4 + 2] = (buffer.vec[i].2.min(1.0).max(0.0) * 255.99).floor() as u8;
-                buf[i * 4 + 3] = (buffer.vec[i].3.min(1.0).max(0.0) * 255.99).floor() as u8;
+        if let Some(frame) = self.current_frame {
+            if let Some(ref render) = self.render {
+                let (width, height) = self.size;
+                let buffer = {
+                    let render = unsafe { std::mem::transmute::<&dyn Render<Rgba>, &'static (dyn Render<Rgba> + Send + Sync)>(render) };
+                    render_to_buffer_parallel(&RenderOpt {
+                        x_range: 0..width as i32,
+                        y_range: 0..height as i32,
+                        res_x: width,
+                        res_y: height,
+                        frame_range: frame..frame+1,
+                        framerate: self.framerate
+                    }, render) as Buffer<Rgba>
+                };
+                let mut buf: Vec<u8> = vec![0; buffer.vec.len() * 4];
+                for i in 0..buffer.vec.len() {
+                    buf[i * 4 + 0] = (buffer.vec[i].0.min(1.0).max(0.0) * 255.99).floor() as u8;
+                    buf[i * 4 + 1] = (buffer.vec[i].1.min(1.0).max(0.0) * 255.99).floor() as u8;
+                    buf[i * 4 + 2] = (buffer.vec[i].2.min(1.0).max(0.0) * 255.99).floor() as u8;
+                    buf[i * 4 + 3] = (buffer.vec[i].3.min(1.0).max(0.0) * 255.99).floor() as u8;
+                }
+                let mut bin = Vec::new();
+                image::png::PNGEncoder::new(&mut bin).encode(&buf, width as u32, height as u32, image::RGBA(8)).unwrap();
+                ctx.text(r#"{"type":"frame"}"#);
+                ctx.binary(bin);
             }
-            let mut bin = Vec::new();
-            image::png::PNGEncoder::new(&mut bin).encode(&buf, width as u32, height as u32, image::RGBA(8)).unwrap();
-            ctx.text(r#"{"type":"frame"}"#);
-            ctx.binary(bin);
+
+            if let Some(ref audio_render) = self.audio_render {
+                let sample_rate = self.samplerate;
+                let ro = kantera::audio_render::AudioRenderOpt {
+                    sample_rate: sample_rate,
+                    sample_range: frame as i64 * sample_rate as i64 / self.framerate as i64..(frame as i64 + 1) * sample_rate as i64 / self.framerate as i64
+                };
+                let vec = audio_render.render(&ro);
+                let mut bin = Vec::new();
+                use std::io::Write;
+                for v in vec.iter() {
+                    let v = ((v.min(1.0).max(-1.0) + 1.0) / 2.0 * std::u16::MAX as f64).round() as u16;
+                    bin.write(&v.to_le_bytes()).unwrap();
+                }
+                ctx.text(r#"{"type":"audio"}"#);
+                ctx.binary(bin);
+            }
+
+            ctx.text(format!(r#"{{"type":"sync","frame":{}}}"#, frame));
+            let frame = frame + 1;
+            self.current_frame = Some(frame);
+            if let Some(end_frame) = self.end_frame {
+                if end_frame <= frame {
+                    if self.loop_ {
+                        self.current_frame = Some(self.start_frame);
+                    } else {
+                        self.current_frame = None;
+                    }
+                }
+            }
         }
 
-        if let Some(ref audio_render) = self.audio_render {
-            let sample_rate = self.samplerate;
-            let ro = kantera::audio_render::AudioRenderOpt {
-                sample_rate: sample_rate,
-                sample_range: frame as i64 * sample_rate as i64 / self.framerate as i64..(frame as i64 + 1) * sample_rate as i64 / self.framerate as i64
-            };
-            let vec = audio_render.render(&ro);
-            let mut bin = Vec::new();
-            use std::io::Write;
-            for v in vec.iter() {
-                let v = ((v.min(1.0).max(-1.0) + 1.0) / 2.0 * std::u16::MAX as f64).round() as u16;
-                bin.write(&v.to_le_bytes()).unwrap();
-            }
-            ctx.text(r#"{"type":"audio"}"#);
-            ctx.binary(bin);
-        }
-
-        ctx.text(format!(r#"{{"type":"sync","frame":{}}}"#, frame));
-        self.frame += 1;
-        if let Some(frame_num) = self.frame_num {
-            if frame_num as i32 <= self.frame {
-                self.frame = 0;
-            }
-        }
-
+        // Schedule next render
         let desire_duration = Duration::from_millis(1000 / self.framerate as u64);
         self.render_at = (self.render_at + desire_duration).max(Instant::now() - desire_duration);
         let duration = self.render_at.checked_duration_since(Instant::now()).unwrap_or(Duration::from_millis(1));
@@ -163,7 +174,9 @@ impl MyWebSocket {
         rt.insert("framerate", r(FRAMERATE_DEFAULT as i32));
         rt.insert("samplerate", r(SAMPLERATE_DEFAULT as i32));
         rt.insert("frame_size", r(vec![r(600 as i32), r(400 as i32)]));
-        rt.insert("frame_num", r(false));
+        rt.insert("start_frame", r(0i32));
+        rt.insert("end_frame", r(false));
+        rt.insert("loop", r(false));
         rt.insert("frame_height", r(400 as i32));
         rt.insert("__rt_cache", self.rt_cache.clone());
         match rt.re(&src) {
@@ -185,8 +198,10 @@ impl MyWebSocket {
                     let height = *vec[1].borrow().downcast_ref::<i32>().unwrap();
                     self.size = (width.max(0) as usize, height.max(0) as usize);
                 }
-                self.frame_num = rt.get("frame_num").and_then(|val| val.borrow().downcast_ref::<i32>().copied()).map(|val| val.max(1) as usize);
-                self.frame = 0;
+                self.start_frame = rt.get("start_frame").and_then(|val| val.borrow().downcast_ref::<i32>().copied()).unwrap_or(0);
+                self.end_frame = rt.get("end_frame").and_then(|val| val.borrow().downcast_ref::<i32>().copied()).map(|val| val.max(1));
+                self.loop_ = rt.get("loop").and_then(|val| val.borrow().downcast_ref::<bool>().copied()).unwrap_or(false);
+                self.current_frame = Some(self.start_frame);
                 ctx.text(format!(r#"{{"type":"streamInfo","framerate":{:?},"samplerate":{:?}}}"#, self.framerate, self.samplerate));
             },
             Err(mes) => ctx.text(format!(r#"{{"type":"parseFailed","error":{:?}}}"#, format!("{}", mes)))
